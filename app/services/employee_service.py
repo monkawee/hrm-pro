6c3b0956e9382395e14b0992ed975991b72eadf7
monkawee <generalmaitri@hotmail.com>
2026-04-18 20:51:01 +0700

    fix function name

--- app/services/employee_service.py
+++ app/services/employee_service.py
@@ -8,7 +8,7 @@ class EmployeeService:
     # --- 1. ส่วนจัดการพนักงาน (CRUD) ---
 
     @staticmethod
-    def 9j(db: Session, skip: int = 0, limit: int = 100):
+    def get_all(db: Session, skip: int = 0, limit: int = 100):
         return db.query(Employee).offset(skip).limit(limit).all()
 
     @staticmethod
