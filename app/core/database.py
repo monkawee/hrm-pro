36b1f99f75473c81cd3058b745cb4d27beb61855
monkawee <generalmaitri@hotmail.com>
2026-05-03 14:07:32 +0700

    edit: update database engine

--- app/core/database.py
+++ app/core/database.py
@@ -11,7 +11,9 @@ if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
     connect_args["check_same_thread"] = False
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
