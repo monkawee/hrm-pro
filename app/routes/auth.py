9b60afa42401b55646f0a5718f57519be9ea98bb
monkawee <generalmaitri@hotmail.com>
2026-05-07 23:32:10 +0700

    remove comment

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -13,35 +13,23 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request, error: Optional[str] = None):
-    # return templates.TemplateResponse(request=request,name="login.html",context={"error": error})
     return templates.TemplateResponse(
         request=request, 
         name="login.html", 
-        context={
-            "user": user,
-            "record": record
-        }
+        context={"error": error}
     )
 
 @router.post("/login")
 async def login(request: Request, username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
     user = UserService.get_user_by_username(db, username)
     if user and UserService.verify_password(password, user.password):
-        # 🌟 1. เก็บ ID ลง Session (Middleware จะจัดการเรื่อง Cookie ให้เองแบบปลอดภัย)
         request.session["user_id"] = user.id 
-        
-        # 🌟 2. Redirect ไป Dashboard ด้วย 303
         return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
-        
-    # return templates.TemplateResponse("login.html", {
-    #     "request": request, 
-    #     "error": "Username หรือ Password ไม่ถูกต้อง"
-    # })
     return templates.TemplateResponse(
         request=request, 
         name="login.html", 
         context={
-            "error": error
+            "error": "Username หรือ Password ไม่ถูกต้อง"
         }
     )
 
